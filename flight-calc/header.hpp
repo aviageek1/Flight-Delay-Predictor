025c6506d24b40658bafcb1878b8f66ae242ca63
Aayush <email2aayush@gmail.com>
2024-06-16 03:25:43 -0400

    test after long time

--- flight-calc/header.hpp
+++ flight-calc/header.hpp
@@ -6,7 +6,7 @@
 int change_in_altitude(int const &end_alt, int const &start_alt);
 
 
-//fpm/glideslope calculator functions
+//fpm and glideslope calculator functions
 int angle(int const &alt_chng, int const &distance);
 std::pair<int,int> fpm(int const &alt_chng, int const &distance, int const &speed);
 
