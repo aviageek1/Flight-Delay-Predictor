efcd4454ce4f1bc7478d205f7f3214aaf9edce59
Aayush <email2aayush@gmail.com>
2024-08-15 00:03:31 -0400

    finished basic model, needs error checking on input

--- flight-delay-predictor/main.py
+++ flight-delay-predictor/main.py
@@ -26,7 +26,7 @@ from datetime import datetime
 
 def open_csv():
     # open and read data file
-    df = pd.read_csv('airlinedelaycauses_DelayedFlights.csv')
+    df = pd.read_csv('flight-delay-predictor/airlinedelaycauses_DelayedFlights.csv')
     return df
 
 
@@ -53,13 +53,13 @@ def origins(o_airports, df): #o_airports is list of airports (ex: o_airports = [
     df = df[df['Origin'].str.contains(keywords)]  # only keep flights originating from keyword airports
     return df
 
-"""
+
 def destinations(d_airports, df):
     # keys = ['MDW', 'LAS', 'OAK', 'PHX', 'SEA'] # only keep flights arriving to keyword airports
     keywords = '|'.join(d_airports)
     df = df[df['Dest'].str.contains(keywords)]
     return df    
-"""
+
 
 """
 def check_empty_rows(df):
@@ -72,28 +72,27 @@ def check_empty_rows(df):
     return df
 """
     
-def quantize(df, delay): # parameter delay is int that states how many minutes counts as "delayed" (ex: 15) 
+def quantize(df, delay, origin, dest): # parameter delay is int that states how many minutes counts as "delayed" (ex: 15) 
     indices_to_drop = []
-
+    filtered_df = df[(df['Origin'] == origin) & (df['Dest'] == dest)]
     # round down and simplify the departure time to only account for the hour
-    for index, row in df.iterrows():
-        df.loc[index, 'CRSDepTime'] = math.floor(row['CRSDepTime'] / 100)
+    for index, row in filtered_df.iterrows():
+        filtered_df.loc[index, 'CRSDepTime'] = math.floor(row['CRSDepTime'] / 100)
         if row['ArrDelay'] <= 0:  # drop flights that had no delay
             indices_to_drop.append(index)
-
         #### change values so "delay"+ minutes late = 1, under "delay" minutes late = 0
         if row['ArrDelay'] >= delay:
-            df.loc[index, 'ArrDelay'] = 1
+            filtered_df.loc[index, 'ArrDelay'] = 1
         else:
-            df.loc[index, 'ArrDelay'] = 0
+            filtered_df.loc[index, 'ArrDelay'] = 0
 
-    df = df.drop(indices_to_drop) # actually remove rows that had no delay 
-
-    return df
+    filtered_df = filtered_df.drop(indices_to_drop) # actually remove rows that had no delay 
+    return filtered_df
 
 # count and print how many occurances of the origin/dest pair there are and how many are delayed
+
 """
-def check_occurances(origin, dest): 
+def check_occurances(origin, dest, df): 
     ######## CHECK OCCURANCES
     # originn = 'SLC'
     # destt = 'DTW'
@@ -108,7 +107,6 @@ def check_occurances(origin, dest):
     count_delay_1 = (filtered_df['ArrDelay'] == 1).sum()
     print(f"Number of rows where Origin is {origin}, Dest is {dest}, and ArrDelay is 1.0 (delayed): {count_delay_1}")
 """
-
 
 def dummy(df):
     # dummify the origin and destination columns
@@ -151,7 +149,7 @@ def predict_delay(departure_date_time, origin, destination, df, model):
 
     try:
         departure_date_time_parsed = datetime.strptime(departure_date_time,
-                                                       '%d/%m/%Y %H:%M:%S')
+                                                       '%m/%d/%Y %H:%M:%S')
     except ValueError as e:
         return f'Error parsing date/time - {e}'
 
@@ -199,36 +197,38 @@ def predict_delay(departure_date_time, origin, destination, df, model):
 
 # driver
 def main():
-    print("Welcome to Flight Model Predictor!")
+    print("\n\n\nWelcome to Flight Model Predictor!")
     print("Created by: Aayush Sharma")
     print("\n")
-    print("Input a major airport's flight route, time, and minutes-delay you want to calculate by and based on 2008 data,"
-          "get a prediction on if it will be delayed or not!")
-    print("Please note that commercial air traffic in 2008 took a big hit in terms of on-time performance, " 
-          "and numbers will likely be lower than expected")
+    print("Input a major airport's flight route, time, and minutes of delay you want to calculate by. \nBased on 2008 data,"
+          " get a prediction on if it will be delayed or not!")
+    print("\nPlease note that commercial air traffic in 2008 took a big hit in terms of on-time performance, " 
+          "and numbers will likely be lower than expected.")
     
+    
+    date = input("\nEnter the flight's departure date in the format mm/dd: ").strip()
+    time = input("Enter the time of the flight in the format hh:mm: ").strip()
+    o_airports = input("Enter an origin airport you would like to search for: ").strip().upper()
+    d_airports = input("Enter a destination airport you would like to search for: ").strip().upper()
+    print("\nLoading.. please wait")
     df = open_csv() # open, read, and create dataframe
     df = remove_extra(df) # clean and simplify dataframe
-    
-    date = input("Enter the flight's departure date in the format mm/dd : ")
-    time = input("Enter the time of the flight in the format hh:mm:ss : ")
-    o_airports = input("Enter an origin airport you would like to search for : ")
-    d_airports = input("Enter a destination airport you would like to search for : ")
-    
+
     df = origins(o_airports, df)
+    df = destinations(d_airports, df)
     
-    delay = input("Enter the amount of delay (in minutes) you would like to sort by (how late is too late?): ")
-    print("Loading.. please wait")
-    df = quantize(df, delay)
+    delay = int(input("\nEnter the amount of delay (in minutes) you would like to sort by (how late is too late?): "))
+    
+    df = quantize(df, delay, o_airports, d_airports)
 
     df = dummy(df)
     df, model, accuracy = trees(df)
 
-    departure_date_time = f"{date}/2008 {time}, {o_airports}, {d_airports}"
-    probability_on_time = predict_delay(departure_date_time, o_airports, d_airports, df, model)
+    departure_date_time = (f"{date}/2008 {time}:00")
+    probability_on_time = (float(predict_delay(departure_date_time, o_airports, d_airports, df, model)))*100
 
-    print(f"The probability that your selected flight will be on-time is {probability_on_time}.")
-    print(f"The model's accuracy is {accuracy}.")
+    print(f"\nThe probability that your selected flight will be on-time is {probability_on_time:.0f}%.")
+    print(f"The model's accuracy is {accuracy}.\n")
 
 
 
