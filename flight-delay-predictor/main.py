99f8a2b1f433811a65a73a43e92e5e58baddf226
Aayush <email2aayush@gmail.com>
2024-08-14 22:09:54 -0400

    sorted into functions

--- flight-delay-predictor/main.py
+++ flight-delay-predictor/main.py
@@ -1,115 +1,153 @@
+##############
+
+#TO-DO: SPLIT INTO FUNCTIONS AND CREATE DRIVER (currently on model)
+#CHECK IF YOU NEED TO RETURN THE df FOR SETTER FUNCTIONS  
+
+##############
+
 import pandas as pd
 import math
 
-######### Scikit Algorithms
+######### Scikit Algorithms #########
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
 
-####### PLOTTING GRAPHS ####
+####### GRAPH PLOTTING #######
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import roc_curve
 
 from datetime import datetime
 
-# open data file
-df = pd.read_csv('airlinedelaycauses_DelayedFlights.csv')
-
-# remove extra columns at end
-df = df.drop(['CarrierDelay', 'WeatherDelay', 'NASDelay', 'SecurityDelay',
-              'LateAircraftDelay'], axis=1)
-
-# remove columns except these
-df = df[["Month", "DayofMonth", "DayOfWeek", "Origin", "Dest", "CRSDepTime",
-         "ArrDelay"]]
-df = df.dropna(how='any')  # clean by dropping any rows with a NaN value
-
-# Biggest hubs for DELTA, AMERICAN, UNITED, SOUTHWEST, ALASKA respectively
-# o_airports = ['ATL', 'DFW', 'ORD', 'DEN', 'SEA']
-o_airports = ['SLC', 'OAK']
-keywords = '|'.join(o_airports)
-
-df = df[df['Origin'].str.contains(
-    keywords)]  # only flights originating from keyword airports
-# keys = ['MDW', 'LAS', 'OAK', 'PHX', 'SEA'] #only these destinations
-# words = '|'.join(keys)
-# df = df[df['Dest'].str.contains(words)]
-print(len(df))  # print number of rows
-
-##### EMPTY CHECKS #####
-# print(df.isnull().sum()) # prints nunmber of empty rows?
-# print(df[df.isnull().values.any(axis=1)].head()) # prints how many empty columns?
 
 
-# df = df.iloc[:100000] # keep only do 1000 rows
+
+def open_csv():
+    # open and read data file
+    df = pd.read_csv('airlinedelaycauses_DelayedFlights.csv')
+    return df
 
 
-indices_to_drop = []
-# round down and simplify the departure time to only account for the hour
-for index, row in df.iterrows():
-    df.loc[index, 'CRSDepTime'] = math.floor(row['CRSDepTime'] / 100)
-    if row['ArrDelay'] <= 0:  # drop flights that had no delay
-        indices_to_drop.append(index)
-    # df.loc[index, 'ArrDelay'] = round(df.loc[index, 'ArrDelay'] / 10) * 10 # quantize values into 10s
+def remove_extra(df):
+    # remove extra columns at end
+    df = df.drop(['CarrierDelay', 'WeatherDelay', 'NASDelay', 'SecurityDelay',
+                'LateAircraftDelay'], axis=1)
 
-    #### change values so 15+ minutes late = 1, under 15 minutes late = 0
-    if row['ArrDelay'] >= 15:
-        df.loc[index, 'ArrDelay'] = 1
-    else:
-        df.loc[index, 'ArrDelay'] = 0
+    # remove columns except these
+    df = df[["Month", "DayofMonth", "DayOfWeek", "Origin", "Dest", "CRSDepTime",
+            "ArrDelay"]]
+    df = df.dropna(how='any')  # clean by dropping any rows with a NaN value
+    return df
 
-df = df.drop(indices_to_drop)
-print(len(df))  # print number of rows
 
-######## CHECK OCCURANCES
+def origins(o_airports, df): #o_airports is list of airports (ex: o_airports = ['SLC', 'OAK'])
+    
+    #########
+    # Biggest hubs for DELTA, AMERICAN, UNITED, SOUTHWEST, ALASKA respectively
+    # o_airports = ['ATL', 'DFW', 'ORD', 'DEN', 'SEA']
+    #########
 
-originn = 'SLC'
-destt = 'DTW'
-# Filter the DataFrame for the specific origin and destination
-filtered_df = df[(df['Origin'] == originn) & (df['Dest'] == destt)]
+    keywords = '|'.join(o_airports)
+    df = df[df['Origin'].str.contains(keywords)]  # only keep flights originating from keyword airports
+    return df
 
-# Get the number of rows
-row_count = len(filtered_df)
+"""
+def destinations(d_airports, df):
+    # keys = ['MDW', 'LAS', 'OAK', 'PHX', 'SEA'] # only keep flights arriving to keyword airports
+    keywords = '|'.join(d_airports)
+    df = df[df['Dest'].str.contains(keywords)]
+    return df    
+"""
 
-print(
-    f"Number of rows where Origin is {originn} and Dest is {destt}: {row_count}")
+"""
+def check_empty_rows(df):
+    ##### EMPTY CHECKS #####
+    # print(df.isnull().sum()) # prints nunmber of empty rows?
+    # print(df[df.isnull().values.any(axis=1)].head()) # prints how many empty columns?
 
-count_delay_1 = (filtered_df['ArrDelay'] == 1).sum()
-print(
-    f"Number of rows where Origin is {originn}, Dest is {destt}, and ArrDelay is 1.0 (delayed): {count_delay_1}")
+    # df = df.iloc[:100000] # keep only do 1000 rows
+    
+    return df
+"""
+    
+def quantize(df, delay): # parameter delay is int that states how many minutes counts as "delayed" (ex: 15) 
+    indices_to_drop = []
 
-# dummify the origin and destination columns
-df = pd.get_dummies(df, columns=['Origin', 'Dest'])
-# print(df.head())
+    # round down and simplify the departure time to only account for the hour
+    for index, row in df.iterrows():
+        df.loc[index, 'CRSDepTime'] = math.floor(row['CRSDepTime'] / 100)
+        if row['ArrDelay'] <= 0:  # drop flights that had no delay
+            indices_to_drop.append(index)
+
+        #### change values so "delay"+ minutes late = 1, under "delay" minutes late = 0
+        if row['ArrDelay'] >= delay:
+            df.loc[index, 'ArrDelay'] = 1
+        else:
+            df.loc[index, 'ArrDelay'] = 0
+
+    df = df.drop(indices_to_drop) # actually remove rows that had no delay 
+
+    return df
+
+# count and print how many occurances of the origin/dest pair there are and how many are delayed
+"""
+def check_occurances(origin, dest): 
+    ######## CHECK OCCURANCES
+    # originn = 'SLC'
+    # destt = 'DTW'
+    # Filter the DataFrame for the specific origin and destination
+    filtered_df = df[(df['Origin'] == origin) & (df['Dest'] == dest)]
+
+    # Get the number of rows
+    row_count = len(filtered_df)
+
+    print(f"Number of rows where Origin is {origin} and Dest is {dest}: {row_count}")
+
+    count_delay_1 = (filtered_df['ArrDelay'] == 1).sum()
+    print(f"Number of rows where Origin is {origin}, Dest is {dest}, and ArrDelay is 1.0 (delayed): {count_delay_1}")
+"""
+
+
+def dummy(df):
+    # dummify the origin and destination columns
+    df = pd.get_dummies(df, columns=['Origin', 'Dest'])
+    # print(df.head())
+
+    return df
+    
+
 
 ############## MODEL
 
-# split into training and testing rows/columns
-train_x, test_x, train_y, test_y = train_test_split(df.drop('ArrDelay', axis=1),
-                                                    df['ArrDelay'],
-                                                    test_size=0.3,
-                                                    random_state=42)
+# returns df, learned model, and accuracy of model
+def trees(df): 
+    # split into training and testing rows/columns
+    train_x, test_x, train_y, test_y = train_test_split(df.drop('ArrDelay', axis=1),
+                                                        df['ArrDelay'],
+                                                        test_size=0.3,
+                                                        random_state=42)
 
-model = RandomForestClassifier(random_state=18)
-model.fit(train_x, train_y)  # make decision trees
+    model = RandomForestClassifier(random_state=18)
+    model.fit(train_x, train_y)  # make decision trees
 
-##### ACCURACY TESTS #####
-# print(model.score(test_x, test_y)) # get model accuracy
+    ##### ACCURACY TESTS #####
+    accuracy = model.score(test_x, test_y) # get model accuracy
 
-probabilities = model.predict_proba(test_x)
+    # probabilities = model.predict_proba(test_x)
+    # print(roc_auc_score(test_y, probabilities[:, 1]))
 
+    # predict = model.predict(test_x)
+    # print(confusion_matrix(test_y,predict))
 
-# print(roc_auc_score(test_y, probabilities[:, 1]))
+    return df, model, accuracy
 
-# predict = model.predict(test_x)
-# print(confusion_matrix(test_y,predict))
 
 
 ###### CREATING PREDICTION ######
 
-def predict_delay(departure_date_time, origin, destination):
+def predict_delay(departure_date_time, origin, destination, df, model):
 
     try:
         departure_date_time_parsed = datetime.strptime(departure_date_time,
@@ -153,6 +191,45 @@ def predict_delay(departure_date_time, origin, destination):
     # if want probability of delay, use [0][1]
 
 
-# Example usage
-print("The probability of the flight being on-time is",
-      predict_delay('07/10/2008 21:45:00', originn, destt))
+# # Example usage
+# print("The probability of the flight being on-time is",
+#       predict_delay('07/10/2008 21:45:00', origin, dest))
+
+
+
+# driver
+def main():
+    print("Welcome to Flight Model Predictor!")
+    print("Created by: Aayush Sharma")
+    print("\n")
+    print("Input a major airport's flight route, time, and minutes-delay you want to calculate by and based on 2008 data,"
+          "get a prediction on if it will be delayed or not!")
+    print("Please note that commercial air traffic in 2008 took a big hit in terms of on-time performance, " 
+          "and numbers will likely be lower than expected")
+    
+    df = open_csv() # open, read, and create dataframe
+    df = remove_extra(df) # clean and simplify dataframe
+    
+    date = input("Enter the flight's departure date in the format mm/dd : ")
+    time = input("Enter the time of the flight in the format hh:mm:ss : ")
+    o_airports = input("Enter an origin airport you would like to search for : ")
+    d_airports = input("Enter a destination airport you would like to search for : ")
+    
+    df = origins(o_airports, df)
+    
+    delay = input("Enter the amount of delay (in minutes) you would like to sort by (how late is too late?): ")
+    print("Loading.. please wait")
+    df = quantize(df, delay)
+
+    df = dummy(df)
+    df, model, accuracy = trees(df)
+
+    departure_date_time = f"{date}/2008 {time}, {o_airports}, {d_airports}"
+    probability_on_time = predict_delay(departure_date_time, o_airports, d_airports, df, model)
+
+    print(f"The probability that your selected flight will be on-time is {probability_on_time}.")
+    print(f"The model's accuracy is {accuracy}.")
+
+
+
+main()
